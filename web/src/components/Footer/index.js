05cb62395f17ac0a5de3269ecd60f889ca5166dc
Djalma Araujo <djalma@close.io>
2017-11-27 17:19:16 +0000

    Correct github link

--- web/src/components/Footer/index.js
+++ web/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => (
   <footer className="text-center">
     <hr />
-    Fichamento Online © <a href="https://github.com/djalmaaraujo/fichamento" title="Github Link">Is Open Source</a>
+    Fichamento Online © <a href="https://github.com/djalmaaraujo/fichamento.online" title="Github Link">Is Open Source</a>
   </footer>
 )
 
